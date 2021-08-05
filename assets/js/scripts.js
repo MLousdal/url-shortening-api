6d5130a912e3c9bd62f239f8583e17bfd6c027d1
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2021-08-05 19:42:32 +0200

    HTML and CSS done!
    Implemented webtemplate, setup semantic HTML, styled with css, made responsive.
    
    Time: 4,25 hrs

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+// Kode til at toggle burger menuen
+function burgerToggle() {
+    const navLinks = document.querySelector("#nav-links");
+    // Hvis navLinks er vist, skjul den da
+    if (navLinks.style.display === "block") {
+        navLinks.style.display = "none";
+    // ellers vis den
+    } else {
+        navLinks.style.display = "block";
+    }
+}
